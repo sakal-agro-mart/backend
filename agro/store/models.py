b415158961675e78fef26d1ac9624cba62262cbf
AabhasDhaubanja <aabhas41@gmail.com>
2022-01-20 11:59:35 +0545

    Basic models and migrations: - Category, Product and Image - Basic Migrations - Data Migration for Category

--- agro/store/models.py
+++ agro/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.PositiveIntegerField()
+    featured = models.BooleanField(default=False)
+    recent = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    link = models.CharField(max_length=200)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
+
+
+class Cart(models.Model):
+    pass
+
+
+class Wishlist(models.Model):
+    pass
